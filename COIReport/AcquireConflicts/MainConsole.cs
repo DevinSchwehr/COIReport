b207fb9192b6b1160f84c6df858067f8784491fc
DEVIN SCHWEHR <devin.schwehr@gmail.com>
2020-06-22 16:46:46 -0600

    Made the method for analyzing the returned OPD list and reporting discrepancies. Will test later. Pushing for progress.

--- COIReport/AcquireConflicts/MainConsole.cs
+++ COIReport/AcquireConflicts/MainConsole.cs
@@ -40,6 +40,12 @@ namespace RedcapApiDemo
                 {
                     string[] dates = author.receiveddate.Split('-');
                     int year = int.Parse(dates[0]);
+                        Thread Thread2018 = new Thread(()=>VoidSearchOPD(2018, author));
+                        Thread Thread2017 = new Thread(()=>VoidSearchOPD(2017, author));
+                        Thread Thread2016 = new Thread(() => VoidSearchOPD(2016, author));
+                        Thread Thread2015 = new Thread(() => VoidSearchOPD(2015, author));
+                        Thread Thread2014 = new Thread(() => VoidSearchOPD(2014, author));
+                        Thread Thread2013 = new Thread(() => VoidSearchOPD(2013, author));
                     if(year== 2018)
                     {
                         //Thread Thread2018 = new Thread(() => SearchOPD(2018, author));
@@ -48,10 +54,6 @@ namespace RedcapApiDemo
                         //results.AddRange(SearchOPD(2017, author));
                         //results.AddRange(SearchOPD(2016, author));
                         //results.AddRange(SearchOPD(2015, author));
-                        Thread Thread2018 = new Thread(()=>VoidSearchOPD(2018, author));
-                        Thread Thread2017 = new Thread(()=>VoidSearchOPD(2017, author));
-                        Thread Thread2016 = new Thread(() => VoidSearchOPD(2016, author));
-                        Thread Thread2015 = new Thread(() => VoidSearchOPD(2015, author));
                         Thread2018.Start();
                         Thread2017.Start();
                         Thread2016.Start();
@@ -63,32 +65,62 @@ namespace RedcapApiDemo
                     }
                     else if(year == 2017)
                     {
-                        results.AddRange(SearchOPD(2017, author));
-                        results.AddRange(SearchOPD(2016, author));
-                        results.AddRange(SearchOPD(2015, author));
-                        results.AddRange(SearchOPD(2014, author));
+                        //results.AddRange(SearchOPD(2017, author));
+                        //results.AddRange(SearchOPD(2016, author));
+                        //results.AddRange(SearchOPD(2015, author));
+                        //results.AddRange(SearchOPD(2014, author));
+                        Thread2017.Start();
+                        Thread2016.Start();
+                        Thread2015.Start();
+                        Thread2014.Start();
+                        Thread2017.Join();
+                        Thread2016.Join();
+                        Thread2015.Join();
+                        Thread2014.Join();
                     }
                     else if(year == 2016)
                     {
-                        results.AddRange(SearchOPD(2016, author));
-                        results.AddRange(SearchOPD(2015, author));
-                        results.AddRange(SearchOPD(2014, author));
-                        results.AddRange(SearchOPD(2013, author));
+                        //results.AddRange(SearchOPD(2016, author));
+                        //results.AddRange(SearchOPD(2015, author));
+                        //results.AddRange(SearchOPD(2014, author));
+                        //results.AddRange(SearchOPD(2013, author));
+                        Thread2016.Start();
+                        Thread2015.Start();
+                        Thread2014.Start();
+                        Thread2013.Start();
+                        Thread2016.Join();
+                        Thread2015.Join();
+                        Thread2014.Join();
+                        Thread2013.Join();
+
                     }
                     else if (year == 2015)
                     {
-                        results.AddRange(SearchOPD(2015, author));
-                        results.AddRange(SearchOPD(2014, author));
-                        results.AddRange(SearchOPD(2013, author));
+                        //results.AddRange(SearchOPD(2015, author));
+                        //results.AddRange(SearchOPD(2014, author));
+                        //results.AddRange(SearchOPD(2013, author));
+                        Thread2015.Start();
+                        Thread2014.Start();
+                        Thread2013.Start();
+                        Thread2015.Join();
+                        Thread2014.Join();
+                        Thread2013.Join();
                     }
                     else if (year == 2014)
                     {
-                        results.AddRange(SearchOPD(2014, author));
-                        results.AddRange(SearchOPD(2013, author));
+                        //results.AddRange(SearchOPD(2014, author));
+                        //results.AddRange(SearchOPD(2013, author));
+                        Thread2014.Start();
+                        Thread2013.Start();
+                        Thread2014.Join();
+                        Thread2013.Join();
+
                     }
                     else if(year == 2013)
                     {
-                        results.AddRange(SearchOPD(2013, author));
+                        //results.AddRange(SearchOPD(2013, author));
+                        Thread2013.Start();
+                        Thread2013.Join();
                     }
                 }
                 catch(Exception e)
@@ -124,10 +156,38 @@ namespace RedcapApiDemo
         //Next step to perform: Analyze the list acquired from the OPD and then compare if all of the companies listed in the OPD match with the companies that they listed.
         static void AnalyzeOPDList(List<String[]> rows, Person author)
         {
-            //These two lines of code get us a list with all of the companies that this person has reported receiving payments from.
-            //After this, we will use the rows that we received to 
-            List<String> authorReportedCompanies = new List<String>(author.companiesNumbered.Split(','));
-            authorReportedCompanies.AddRange(author.otherCompanies.Split(','));
+            List<String> authorCompanies = new List<String>(author.otherCompanies.Split(','));
+            string[] companyNumbers = author.companiesNumbered.Split(',');
+            //Since everything is in numbers, we go through a loop of converting the numbers into companies from the data dictionaries
+            //and then we add them to the list of all of the companies the author reported.
+            for(int i=0; i<companyNumbers.Length; i++)
+            {
+                authorCompanies.Add(GetData.companyDictionary[int.Parse(companyNumbers[i])]);
+            }
+            //Now we begin to analyze the reported companies with the name matches from the OPD
+            foreach(string[] OPDEntry in rows)
+            {
+                //If there is an OPD entry that has a company that is not in the list of companies the author reported,
+                //Then there is a discrepancy that must be shown to the user.
+                if (!(authorCompanies.Contains(OPDEntry[25])))
+                {
+                    Console.WriteLine("DISCREPANCY: Company not reported by author. Company is: " + OPDEntry[25] + ", Date of payment: " + OPDEntry[31] + "Type of Payment: " + OPDEntry[34]);
+                }
+                //If the above statement is false, then we can know that there was a match, and there was no discrepancy
+                else
+                {
+                    //We then remove that company from the list. This will help us later to print out all of the companies that the author reported that were not found in the OPD
+                    authorCompanies.Remove(OPDEntry[25]);
+                }
+            }
+            //After the loop, we check the remaining companies reported by the author. If there are any, we report to the user that these companies were not found in the OPD.
+            if(authorCompanies.Count > 0)
+            {
+                foreach(string company in authorCompanies)
+                {
+                    Console.WriteLine("DISCREPANCY: Company " + company + " was reported by the author, but not found within the OPD.");
+                }
+            }
         }
     }
 }
