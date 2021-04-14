e11a7244aeb4b6420b9c4f1e64548b6875df5207
DEVIN SCHWEHR <u1205752@umail.utah.edu>
2021-04-14 15:24:56 -0600

    Finished working with Ophth Real. moving on to JAMA Real.

--- COIReport/AcquireConflicts/MainConsole.cs
+++ COIReport/AcquireConflicts/MainConsole.cs
@@ -16,7 +16,7 @@ namespace RedcapApiDemo
         static List<String[]> searchResults = new List<String[]>();
         static string companyHitString; 
         static string companyOutputFilePath = @"D:\Users\u1205752\Documents\COI\OphthReal2.csv";
-        static string authoroutputFilePath = @"D:\Users\u1205752\Documents\COI\OphthRealTest2.csv";
+        static string authoroutputFilePath = @"D:\Users\u1205752\Documents\COI\OphthRealAuthor2.csv";
 
         static void Main(string[] args)
         {
@@ -35,7 +35,7 @@ namespace RedcapApiDemo
             
             Console.WriteLine("Successfully acquired Authors and Data Dictionary.");
             //foreach(Person author in authors)
-            for (int i = 0; i < authors.Count; i++)
+            for (int i = 486; i < authors.Count; i++)
             {
                 searchResults = new List<String[]>();
 
